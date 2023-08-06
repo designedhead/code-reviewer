61602a30968b0ebd30a05adfd85d43b9fb3af233
Rafael Mendes <30901317+designedhead@users.noreply.github.com>
2023-08-06 19:23:26 +0100

    feat: add loader

--- code-review.js
+++ code-review.js
@@ -7,6 +7,7 @@ const chalk = require("chalk");
 // functions
 const fetchOpenAi = require("./utils/openai");
 const styleDynamicText = require("./utils/styledLogs");
+const ora = require("ora");
 
 // Function to execute shell commands
 const execShellCommand = (command) => {
@@ -24,6 +25,8 @@ const execShellCommand = (command) => {
 // Function to get the staged file paths
 const getStagedFiles = async () => {
   try {
+    const spinner = ora("Getting a review...").start();
+
     // Get the list of staged files using Git command
     const stagedFiles = await execShellCommand(
       "git diff --name-only HEAD | xargs"
@@ -44,24 +47,37 @@ const getStagedFiles = async () => {
           path.endsWith(".tsx")
       );
 
-    // Read the contents of each staged file
-
-    for (const filePath of stagedFilesArray) {
-      fs.readFile(filePath, "utf8", async (err, content) => {
-        if (err) {
-          console.error(`Error reading file: ${filePath}`, err);
-          return;
-        }
-
-        const res = await fetchOpenAi(
-          `File path is:${filePath}, file content is: ${content}`
-        );
-
-        console.log(chalk.blue.bgBlack.bold(`File ${filePath}:\n`));
-        console.log(styleDynamicText(res));
-        console.log("\n\n\n");
+    // Use Promise.all to wait for all the file reading operations to complete
+    const readPromises = stagedFilesArray.map((filePath) => {
+      return new Promise((resolve, reject) => {
+        fs.readFile(filePath, "utf8", (err, content) => {
+          if (err) {
+            console.error(`Error reading file: ${filePath}`, err);
+            reject(err);
+          } else {
+            resolve({ filePath, content });
+          }
+        });
       });
+    });
+
+    // Wait for all file reading promises to resolve
+    const results = await Promise.all(readPromises);
+
+    // Process the contents and display the results
+    for (const { filePath, content } of results) {
+      const res = await fetchOpenAi(
+        `File path is:${filePath}, file content is: ${content}`
+      );
+
+      console.log(chalk.blue.bgBlack.bold(`File ${filePath}:\n`));
+      console.log(styleDynamicText(res));
+      console.log("\n\n\n");
     }
+
+    // Stop showing the loading message
+    console.log("finished");
+    spinner.succeed("Task completed!");
   } catch (error) {
     console.error("Error retrieving staged files:", error);
   }
